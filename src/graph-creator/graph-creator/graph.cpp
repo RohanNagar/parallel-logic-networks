e03d16d470906db39072934a9a797363c347cbf9
Ben Fu <ben_fu1@yahoo.com>
2016-11-13 00:37:54 -0600

    Change adjacency list representation from map to vector

--- src/graph-creator/graph-creator/graph.cpp
+++ src/graph-creator/graph-creator/graph.cpp
@@ -5,16 +5,20 @@
  * Create an empty ordered map.
  */
 pln::graph::graph() {
-    m_graph = std::map<vertex_t, std::vector<vertex_t>>();
+    m_graph = std::vector<std::vector<vertex_t>>{};
 }
 
 void pln::graph::insert_vertex(vertex_t id) {
-    std::vector<vertex_t> adj_list;
-    m_graph.insert({ id, adj_list });
+    std::vector<vertex_t> adj_vertices;
+    m_graph.push_back(adj_vertices);
 }
 
 void pln::graph::insert_edge(vertex_t src, vertex_t dest) {
-    m_graph.find(src);
+    if (src > m_graph.size()) {
+        perror("Vertex does not exist.");
+        exit(1);
+    }
+    m_graph[src].push_back(dest);
 }
 
 void pln::graph::set_heights(std::vector<vertex_t> const& start_vertices) {
