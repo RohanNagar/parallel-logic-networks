bb2116b81ef527ca494f979e3eab5937e1a49241
Rohan Nagar <rohan.nagar@utexas.edu>
2016-11-14 17:42:28 -0600

    Add sequence generator script

--- scripts/README.md
+++ scripts/README.md
@@ -1,5 +1,7 @@
 # How to Run
 
+## `parser.py`
+
 1. Make sure you are in the `scripts/` directory.
 
 2. Run the following command.
@@ -9,4 +11,16 @@ $ python parser.py -f INPUT_FILENAME -o OUTPUT_FILENAME
 ```
 
 The input file is designated by the `-f` flag, and the output file is designated by the `-o` flag.
+
+## `generate_sequences.py`
+
+1. Make sure you are in the `scripts/` directory.
+
+2. Run the following command.
+
+```bash
+$ python generate_sequences.py -n NUM_INPUTS -f OUTPUT_FILENAME
+```
+
+The number of inputs (also the length of the binary strings to be generated) is designated by the `-n` flag, and the output filename is designated by the `-f` flag.
 
---
+++ scripts/generate_sequences.py
@@ -0,0 +1,23 @@
+import argparse
+
+from itertools import product
+
+
+def main(n, outfile):
+    sequences = [''.join(seq) for seq in product('01', repeat=n)]
+
+    with open(outfile, 'w') as f:
+        f.write('\n'.join(seq for seq in sequences))
+
+
+if __name__ == '__main__':
+    # Command line arguments
+    parser = argparse.ArgumentParser('Binary String Generator')
+    parser.add_argument('-n', '--length', type=int, required=True,
+            help='the length of the binary strings to generate')
+    parser.add_argument('-f', '--filename', type=str, required=True,
+            help='the name of the file to create')
+
+    args = parser.parse_args()
+    main(args.length, args.filename)
+
